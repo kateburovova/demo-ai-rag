29bd6a5a49c90fe67f262be934af15690ff634c4
Kate <kate.burovova@gmail.com>
2024-08-05 13:52:44 +0300

    fix pull prompts

--- utils.py
+++ utils.py
@@ -119,10 +119,11 @@ def pull_prompts(config):
 
     for task, task_config in config['tasks'].items():
         prompt_id = task_config['primary']
-        full_prompt_id = f"{langsmith_acc}/{prompt_id}"
+        full_prompt_url = f'https://api.hub.langchain.com/commits/{langsmith_acc}/{prompt_id}'
+        # full_prompt_id = f"{langsmith_acc}/{prompt_id}"
 
         try:
-            prompt_template = hub.pull(full_prompt_id)
+            prompt_template = hub.pull(full_prompt_url)
             prompts[task] = prompt_template
             logging.info(f"Successfully pulled prompt for task: {task}")
         except Exception as e:
